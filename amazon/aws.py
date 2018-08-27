2b67e212d877d207efde7765d755c97e44948350
Williano <willyudi@gmail.com>
2018-08-26 23:00:45 -0300

    Leitura CSV

--- amazon/aws.py
+++ amazon/aws.py
@@ -1,2 +1,13 @@
-#testando github sábado
-print ("Hello World")
+#!/usr/bin/env python
+# encoding: utf-8
+import csv
+#Incidentes
+#SAT;Serviço;Título;Solicitante;Superior Imediato;Empresa;Departamento;Sistema Afetado;Gestor do Sistema;Empresa do Sistema;Departamento do Sistema;Estado;Analista Responsável;Executor;Data de Abertura;Data de Atribuição;Data de Transferência;Data de Pedido de Parecer;Data de Retorno de Parecer;Data Efetiva de Conclusão;Coordenador;Motivo do Encerramento
+#22 colunas
+with open('incidentes.csv', 'r', newline='', encoding='mac_cyrillic') as csv_file:
+	csv_reader = csv. reader(csv_file)
+	contador = 1
+	for line in csv_reader:
+		print(str(contador) + ' - ' + str(line))
+		contador = contador + 1
+		
